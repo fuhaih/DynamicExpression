89f2645f38b364185982e2cae5a4f73edeaa44d7
fuhaih <1154590036@qq.com>
2022-08-23 20:44:53 +0800

    feat:测试代码

--- src/DynamicExpression.Demo/Program.cs
+++ src/DynamicExpression.Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace DynamicExpression.Demo
 {
@@ -6,11 +7,28 @@ namespace DynamicExpression.Demo
     {
         static void Main(string[] args)
         {
+            TestExpression1();
+            Console.ReadLine();
+        }
+
+        public static void TestExpression()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             ExpressionCompiler expressionCompiler = new ExpressionCompiler();
             expressionCompiler.SetParameter<int>("value");
-            var func = expressionCompiler.Compile("value+2");
+            var func = expressionCompiler.Compile("{value = value + 2;return value;}");
             var result = func.DynamicInvoke(3);
-            Console.ReadLine();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        }
+        public static void TestExpression1()
+        {
+            //PointerType
+            //value1*value2 会被判断为是指针类型的变量定义 LocalDeclarationStatement
+            //类型为value1* 名称为value2
+            ExpressionCompiler expressionCompiler = new ExpressionCompiler();
+            var func = expressionCompiler.Compile("(34-8)-(12-2)*2");
+            var result = func.DynamicInvoke();
         }
     }
 }
