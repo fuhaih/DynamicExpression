a7cccdfd1d6d69d55dad7b4e9c0da1c0c58a5356
fuhaih <1154590036@qq.com>
2022-08-23 21:06:33 +0800

    feat:添加块表达式测试用例

---
+++ test/DynamicExpression.Test/ExpressionBlockTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicExpression.Test
+{
+    public class ExpressionBlockTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("{value1 = value1+value2;return value1;}", 3,4,7)]
+        public void Block_WithParameter(string expression,object value1,object value2, object result)
+        {
+            ExpressionCompiler expressionCompiler = new ExpressionCompiler();
+            expressionCompiler.SetParameter(value1.GetType(), "value1");
+            expressionCompiler.SetParameter(value2.GetType(), "value2");
+            var func = expressionCompiler.Compile("value1+value2");
+            var assert = func.DynamicInvoke(value1, value2);
+            Assert.AreEqual(result, assert);
+        }
+    }
+}
